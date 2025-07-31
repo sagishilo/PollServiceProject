169c85b80ae1fb2c8ecb33b71866ef5608810ae9
Sagi Shilo <sagishilo1@gmail.com>
2025-07-31 15:25:26 +0300

    new divs

---
+++ resources/db-migrations/init.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS customer_order;
+DROP TABLE IF EXISTS customer_favorite_item;
+
+CREATE TABLE customer(
+id INT(11) NOT NULL AUTO_INCREMENT,
+first_name VARCHAR (300) NOT NULL DEFAULT "",
+last_name VARCHAR (300) NOT NULL DEFAULT "",
+email VARCHAR (300) NOT NULL DEFAULT "",
+status VARCHAR (300) NOT NULL DEFAULT "",
+PRIMARY KEY (id)
+);
+
+CREATE TABLE customer_order(
+id INT(11) NOT NULL AUTO_INCREMENT,
+customer_id INT(11) NOT NULL,
+item_name VARCHAR (300) NOT NULL DEFAULT "",
+price DECIMAL(10,2) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (customer_id)
+);
+
+CREATE TABLE customer_favorite_item(
+id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+customer_id INT(11) NOT NULL,
+item_id INT(11) NOT NULL,
+FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
